9059a700530f5a53a13da9aad6c70375c06bb06e
3jun <kk3june@gmail.com>
2022-02-20 10:57:43 +0900

    FIX: rebase conflict

--- index.js
+++ index.js
@@ -3,19 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import App from './list';
-=======
-import App from './splash';
->>>>>>> e40b6f0 (ADD: Splash 페이지 레이아웃 구현)
-=======
 import App from './App';
->>>>>>> 15e70e9 (ADD: install react-native-navigation library and dependencies)
-=======
-import App from './App';
->>>>>>> c60ca5a (MERGE: branch dev into list)
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
